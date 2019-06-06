1f7ae96a6ea4a7707874190eb5881a1d433106df
liuyu <12345678>
2019-06-06 11:20:10 +0800

    添加配置

--- src/main/java/com/config/service/MarketConfigServiceApplication.java
+++ src/main/java/com/config/service/MarketConfigServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class MarketConfigServiceApplication {
 
     public static void main(String[] args) {
